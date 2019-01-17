fa2cf2fd4768392b89574bc31b269ed0f63a7448
kodoj <kodoj1@o2.pl>
2019-01-17 19:01:14 +0100

    initial Login and Web servlets, web.xml filled

---
+++ src/main/java/com/codecool/myApp/servlets/LoginServlet.java
@@ -0,0 +1,4 @@
+package com.codecool.servlets;
+
+public class LoginServlet {
+}
---
+++ src/main/java/com/codecool/myApp/servlets/WebServlet.java
@@ -0,0 +1,17 @@
+package com.codecool.servlets;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+
+public class WebServlet extends HttpServlet {
+
+    public void service(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+
+        // do something in here
+    }
+}
